bfcb8b7eb6bf07bdc92710425fe8ede660ce6479
alvaroperez <alvaroperez@ALVAROPEREZB295>
2023-04-26 12:13:31 -0500

    Agregar archivos de proyecto.

---
+++ StyleApp/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StyleApp.Models;
+
+namespace StyleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<User>());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new User());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] User user)
+        {
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User user)
+        {
+
+            return Ok(user);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] User user)
+        {
+            if (user.Name != "")
+            {
+                return Ok(new User());
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+
+    }
+
+
+}
